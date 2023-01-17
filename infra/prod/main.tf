f0b0db1e7e490e03889040919b60808f64e4c3c6
Rodrigo Rahal <rrrahal19@gmail.com>
2023-01-17 14:08:43 -0300

    infra: add prod codedeploy

--- infra/prod/main.tf
+++ infra/prod/main.tf
@@ -5,3 +5,11 @@ module "lambda_function" {
 
   function_name = "poc_pipelines_lambda"
 }
+
+module "codedeploy" {
+  source = "../modules/codedeploy"
+
+  env = "prod"
+
+  deployment_config_name = "CodeDeployDefault.LambdaLinear10PercentEvery1Minute"
+}
