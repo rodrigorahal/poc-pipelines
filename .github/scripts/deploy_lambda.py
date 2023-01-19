338c0dbd27ac033b93ddec8791a5c02214e89962
Rodrigo Rahal <rrrahal19@gmail.com>
2023-01-19 11:26:01 -0300

    github actions: use iam role instead of iam user:v5

--- .github/scripts/deploy_lambda.py
+++ .github/scripts/deploy_lambda.py
@@ -5,8 +5,8 @@ import sys
 import time
 
 
-lambda_client = boto3.client('lambda')
-codedeploy_client = boto3.client('codedeploy')
+lambda_client = boto3.client("lambda")
+codedeploy_client = boto3.client("codedeploy")
 
 
 def make_zip_file_bytes(path):
@@ -18,34 +18,39 @@ def make_zip_file_bytes(path):
 def update_function_code(function_name):
     response = lambda_client.update_function_code(
         FunctionName=function_name,
-        ZipFile=make_zip_file_bytes('./app.zip'),
+        ZipFile=make_zip_file_bytes("./app.zip"),
         Publish=True,
     )
     target_version = response["Version"]
     return target_version
 
+
 def get_alias_current_version(function_name):
     response = lambda_client.get_alias(
-        FunctionName=function_name,
-        Name=f"{function_name}_alias"
+        FunctionName=function_name, Name=f"{function_name}_alias"
     )
     current_version = response["FunctionVersion"]
     return current_version
 
-def create_deployment(application_name, deployment_group_name, current_version, target_version):
+
+def create_deployment(
+    application_name, deployment_group_name, current_version, target_version
+):
     app_spec = {
         "version": 0.0,
-        "Resources": [{
-            function_name: {
-                "Type": "AWS::Lambda::Function",
-                "Properties": {
-                    "Name": function_name,
-                    "Alias": f"{function_name}_alias",
-                    "CurrentVersion": current_version,
-                    "TargetVersion": target_version
+        "Resources": [
+            {
+                function_name: {
+                    "Type": "AWS::Lambda::Function",
+                    "Properties": {
+                        "Name": function_name,
+                        "Alias": f"{function_name}_alias",
+                        "CurrentVersion": current_version,
+                        "TargetVersion": target_version,
+                    },
                 }
             }
-        }]
+        ],
     }
 
     response = codedeploy_client.create_deployment(
@@ -53,14 +58,13 @@ def create_deployment(application_name, deployment_group_name, current_version, 
         deploymentGroupName=deployment_group_name,
         revision={
             "revisionType": "String",
-            "string": {
-               "content": json.dumps(app_spec)
-            }
-        }
+            "string": {"content": json.dumps(app_spec)},
+        },
     )
 
     deployment_id = response["deploymentId"]
     return deployment_id
+
 
 def poll_deployment_status(deployment_id):
     timeout = time.time() + 15 * 60
@@ -72,22 +76,24 @@ def poll_deployment_status(deployment_id):
             return
         elif status in ("Failed", "Stopped"):
             raise ValueError(f"Deployment: {deployment_id} failed")
-        
+
         time.sleep(60)
-    
+
     raise ValueError(f"Timeout while getting deployment: {deployment_id} status")
-        
+
 
 if __name__ == "__main__":
     function_name = sys.argv[1]
-    application_name= sys.argv[2]
+    application_name = sys.argv[2]
     deployment_group_name = sys.argv[3]
 
     target_version = update_function_code(function_name)
     print(f"Target version: {target_version}")
     current_version = get_alias_current_version(function_name)
     print(f"Current version: {current_version}")
-    deployment_id = create_deployment(application_name, deployment_group_name, current_version, target_version)
+    deployment_id = create_deployment(
+        application_name, deployment_group_name, current_version, target_version
+    )
     print(f"Deployment id: {deployment_id}")
 
     poll_deployment_status(deployment_id)
