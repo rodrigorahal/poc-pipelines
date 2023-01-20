a7f0fbbea7b5ec37041c470b652f70b944e9190f
Rodrigo Rahal <rrrahal19@gmail.com>
2023-01-20 11:36:08 -0300

    infra: ignore lambda changes

--- app/index.py
+++ app/index.py
@@ -1,6 +1,6 @@
 def lambda_handler(event, context):
     if "a" not in event or "b" not in event:
-        raise ValueError("a and b are required")
+        raise ValueError("a and b are required params")
 
     a = event["a"]
     b = event["b"]
